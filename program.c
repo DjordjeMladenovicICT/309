4b70a230cb3ab595195348d016a2814cf60616f7
DjordjeMladenovicICT <djordje.mladenovic@ict.edu.rs>
2026-04-08 12:41:54 +0200

    Izmenio sam kod

--- program.c
+++ program.c
@@ -2,9 +2,9 @@
 
 int main()
 {
+    int a,b,c;
 printf("Dobro dosli na povezivanje sa Git-hubom\n");
 printf("Dobro dosli na povezivanje sa Git-hubom\n");
-printf("Dobro dosli na povezivanje sa Git-hubom\n");
-  
+
   
 }
